1c2c486f26598e34847b32133d4e3a9b1aadd7a0
Daniel <daniel-yim@live.com>
2014-06-06 15:55:06 +0800

    Create Client Project for C#

---
+++ com.51xmy.GameClient/Default.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace com._51xmy.GameClient
+{
+    public partial class Default : Form
+    {
+        public Default()
+        {
+            InitializeComponent();
+        }
+    }
+}
